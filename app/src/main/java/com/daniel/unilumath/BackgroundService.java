7b17bcb9e045247b7b55540fa3ae286ea20b420e
Daniel Assani <devasddaniel@gmail.com>
2022-09-09 08:13:16 +0200

    addding service

---
+++ app/src/main/java/com/daniel/unilumath/BackgroundService.java
@@ -0,0 +1,60 @@
+package com.daniel.unilumath;
+
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+
+public class BackgroundService extends Service {
+    private  BackgroundService worker;
+
+    public BackgroundService() {
+    }
+    private int state=0;
+    private final IBinder mBinder = new BackgroundBinder();
+
+    public class BackgroundBinder extends Binder {
+        BackgroundService getService() {
+            return BackgroundService.this;
+        }
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return mBinder;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+// Placez ici le code qui sera exécuté lors de la création de ce service
+    }
+
+    public void working(){
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                state++;
+                //mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+                state = 100;
+            }
+        }.start();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+// Placez ici le code qui sera exécuté à chaque démarrage du service
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+// Placez ici le code qui sera exécuté lors de la destruction de ce service
+    }
+
+}
--- app/src/main/java/com/daniel/unilumath/Etudiant.java
+++ app/src/main/java/com/daniel/unilumath/Etudiant.java
@@ -1,19 +1,29 @@
 package com.daniel.unilumath;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Etudiant extends AppCompatActivity {
-
+    private BackgroundService worker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_etudiant2);
         this.event();
+        this.send("");
     }
     protected void event(){
         Button horraire = (Button)findViewById(R.id.horaire);
@@ -68,4 +78,49 @@ public class Etudiant extends AppCompatActivity {
             }
         });
     }
+
+    public void send(String Data){
+        Intent background = new Intent(Etudiant.this, BackgroundService.class);
+        bindService(background, maConnexion, Context.BIND_AUTO_CREATE);
+        new CountDownTimer(10000, 100) {
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            public void onTick(long millisUntilFinished) {
+                //mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+                ProgressBar  prog = (ProgressBar)findViewById(R.id.progressBar);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    prog.setProgress(prog.getProgress()+1, true);
+                }else{
+                    prog.setProgress(prog.getProgress()+1);
+                }
+                if(prog.getProgress()>=100){
+                    TextView txt = (TextView)findViewById(R.id.textView76);
+                    TextView academic = (TextView)findViewById(R.id.textView20);
+                    academic.setText("2021-2022");
+                    txt.setText("Complete");
+                }
+            }
+
+            public void onFinish() {
+                //mTextField.setText("done!");
+                TextView txt = (TextView)findViewById(R.id.textView76);
+                TextView academic = (TextView)findViewById(R.id.textView20);
+                academic.setText("2021-2022");
+                txt.setText("Complete");
+
+            }
+        }.start();
+    }
+    // L’objet ServiceConnection gère la connexion entre l’activité et le service.
+    private ServiceConnection maConnexion = new ServiceConnection() {
+        // Méthode appelée lorsque la connexion est établie. Récupère la référence
+// vers le service associé.
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            worker = ((BackgroundService.BackgroundBinder)service).getService();
+        }
+        // Méthode appelée lorsque la connexion est rompue.
+        public void onServiceDisconnected(ComponentName className) {
+            worker = null;
+        }
+    };
 }
